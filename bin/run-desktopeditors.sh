e0a028cb9a4c5bd7d84b490e6ef10ef8df211d7f
Roman Demidov <roman.demidov@onlyoffice.com>
2023-02-10 11:15:06 +0300

    Update to version 7.3.0

--- bin/run-desktopeditors.sh
+++ bin/run-desktopeditors.sh
@@ -125,11 +125,6 @@ if [ ! -d "$XDG_RUNTIME_DIR" ]; then
 	mkdir -p $XDG_RUNTIME_DIR -m 700
 fi
 
-#Copy ibus files for input methods
-#if [ -d "$HOME/.config/ibus" ]; then
-#  cp -r $HOME/.config/ibus $XDG_CONFIG_HOME
-#fi
-
 #Create cache for input methods
 export GTK_IM_MODULE_FILE=$XDG_CACHE_HOME/immodules.cache
 if [ ! -f $GTK_IM_MODULE_FILE ]; then
